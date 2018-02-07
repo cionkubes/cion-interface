f7d12c793cac3b37d4381f5d1b15131017837456
Erlend <erlentob@stud.ntnu.no>
2018-02-07 18:19:27 +0100

    Pull dependency from repository

--- setup.py
+++ setup.py
@@ -4,4 +4,10 @@ setup(
     version="0.1",
     license="GPLv3",
     packages=find_packages(),
+    install_requires=[
+        'workq'
+    ],
+    dependency_links=[
+        'git+https://github.com/cionkubes/workq'
+    ]
 )
